0bd067dad6a9e9126bbe4aa39510b5ee4fc65807
Borodur <neveric@protonmail.com>
2022-06-10 15:16:16 +0300

    фиксы импорты

--- handlers/start_hand.py
+++ handlers/start_hand.py
@@ -1,20 +1,15 @@
-import asyncio
-import pathlib
-
 from aiogram import Router, F
 from aiogram import types
 from aiogram.dispatcher.fsm.context import FSMContext
-from aiogram.types import Message, FSInputFile
+from aiogram.types import Message
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 import bata
 from DBuse import data_getter
-
 from handlers.admin_hand import admin_home
-from states.donbass_states import donbass_state
 from keyboards.admin_keys import main_admin_keyboard
-from keyboards.test_keys import strange_kb
-
+from states.antiprop_states import propaganda_victim
+from states.donbass_states import donbass_state
 
 router = Router()
 
@@ -25,18 +20,23 @@ async def cmd_start(message: Message):
     nmarkup.row(types.KeyboardButton(text="8 лет Украина бомбила Донбасс и убивала там детей"))
     await message.answer("Здравствуйте. Вы решили поговорить о Донбассе, не так ли?",
                          reply_markup=nmarkup.as_markup(resize_keyboard=True,
-                         input_field_placeholder="Кстати, я разместил тут разные фразы поддержки и советы, как вам?"))
+                                                        input_field_placeholder="Кстати, я разместил тут разные фразы поддержки и советы, как вам?"))
 
 
 @router.message(F.from_user.id.in_(bata.all_data().admins), commands=["admin"])
 async def admin_hi(message: Message, state: FSMContext) -> None:
     await state.clear()
     await state.set_state(admin_home.admin)
-    await message.answer("Добро пожаловать в режим администрации. Что вам угодно сегодня?", reply_markup=main_admin_keyboard())
+    await message.answer("Добро пожаловать в режим администрации. Что вам угодно сегодня?",
+                         reply_markup=main_admin_keyboard())
+
 
 @router.message(commands=["test"])
 async def admin_hi(message: Message, state: FSMContext) -> None:
-    await message.answer("Сообщение с текстом", reply_markup=strange_kb())
+    nmarkup = ReplyKeyboardBuilder()
+    nmarkup.row(types.KeyboardButton(text="Поехали"))
+    await state.set_state(propaganda_victim.start)
+    await message.answer("Сообщение с текстом")
 
 
 @router.message(text_contains=('бомбила', '8', 'лет'), content_types=types.ContentType.TEXT, text_ignore_case=True)
@@ -49,4 +49,5 @@ async def eight_years_point(message: Message, state=FSMContext):
     nmarkup.row(types.KeyboardButton(text="Да, знал"))
     nmarkup.row(types.KeyboardButton(text="Нет, не знал"))
     nmarkup.adjust(1, 2)
-    await message.answer_photo(photo_id, caption=text, reply_markup=nmarkup.as_markup(resize_keyboard=True, input_field_placeholder="Найдите по-настоящему независимые источники"))
+    await message.answer_photo(photo_id, caption=text, reply_markup=nmarkup.as_markup(resize_keyboard=True,
+                                                                                      input_field_placeholder="Найдите по-настоящему независимые источники"))
