35f498e90e7cd7d65192bc36d643255c89d61739
White <maksfundd2@gmail.com>
2022-06-10 13:13:17 +0300

    add start message

--- Testbot.py
+++ Testbot.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 import pandas as pd
 from sqlalchemy import create_engine
-
+from data_base import db
 from time import sleep
 
 import psycopg2
@@ -15,31 +15,10 @@ from bata import all_data
 from handlers import admin_hand, start_hand
 from handlers.donbass_handlers import select_handler, option_two_hand, option_three_hand, option_four_hand, \
     option_five_hand, option_six_hand, option_seven_hand, option_eight_hand, final_donbass_hand
-
+from handlers.started_message import welcome_messages
 log = logging.getLogger(__name__)
 log.addHandler(logging.FileHandler('logfile.log'))
 
-try:
-    con = psycopg2.connect(database="postgres", user="postgres", password="postgres", host="localhost")
-    con.autocommit = True
-    cursor = con.cursor()
-    cursor.execute("DROP TABLE if exists texts")
-    cursor.execute("DROP TABLE if exists assets")
-    cursor.close()
-    con.close()
-    df1 = pd.read_csv('resources/texts.csv')
-    df2 = pd.read_csv('resources/assets.csv')
-
-    engine = create_engine('postgresql://postgres:postgres@localhost:5432/postgres')
-
-    df1.to_sql("texts", engine)
-    df2.to_sql("assets", engine)
-
-except (Exception, Error) as error:
-    print("SQL EXEPTION ", error)
-
-else:
-    print("Database base created!")
 
 
 async def main():
@@ -54,6 +33,7 @@ async def main():
     )
     log.error("Starting bot")
 
+    dp.include_router(welcome_messages)
     dp.include_router(start_hand.router)
     dp.include_router(admin_hand.router)
     dp.include_router(select_handler.router)
