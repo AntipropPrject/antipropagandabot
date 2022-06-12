da1388263e0efe954e94bc41ea1f2442343078f4
Borodur <neveric@protonmail.com>
2022-06-12 20:16:10 +0300

    болванка для тв ветки

--- keyboards/map_keys.py
+++ keyboards/map_keys.py
@@ -10,3 +10,15 @@ def antip_why_kb():
     nmarkup.row(types.KeyboardButton(text="Поговорим про войну в Украине"))
     nmarkup.row(types.KeyboardButton(text="Но ведь на войне первая жертва -- это правда. Откуда ты знаешь, кому можно верить?"))
     return markup.as_markup(resize_keyboard=True)
+
+
+def antip_killme_kb():
+    nmarkup = ReplyKeyboardBuilder()
+    nmarkup.row(types.KeyboardButton(text="Честно говоря, я в шоке"))
+    nmarkup.row(types.KeyboardButton(text="Сильно удивлен"))
+    nmarkup.row(types.KeyboardButton(text="Немного удивлен"))
+    nmarkup.row(types.KeyboardButton(text="Не удивлен"))
+    nmarkup.row(types.KeyboardButton(text="Я и так знал, что по ТВ врут"))
+    nmarkup.row(types.KeyboardButton(text="Хочу посмотреть еще!"))
+    nmarkup.adjust(1,2,1,1,1)
+    return nmarkup.as_markup(resize_keyboard=True)
--- states/antiprop_states.py
+++ states/antiprop_states.py
@@ -3,3 +3,9 @@ from aiogram.dispatcher.fsm.state import State, StatesGroup
 
 class propaganda_victim(StatesGroup):
     start = State()
+    tv_first = State()
+    tv_russia24 = State()
+    tv_russia1 = State()
+    tv_HTB = State()
+    tv_star = State()
+    tv_ren = State()
