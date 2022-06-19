d3ed955d7719bfc48dc21c6935e788bec2d61184
s.kalashnikov <s.kalashnikov@btfcom.com>
2022-06-19 13:34:36 +0300

    1.1.6

--- bata.py
+++ bata.py
@@ -9,7 +9,7 @@ class all_data():
         self.redis_url = 'redis://localhost:2342'
         self.postgres_data = 'dbname=postgres user=postgres password=postgres port=5432'
         self.mongodb_data = 'mongodb://localhost:27017'
-        self.bot_token = '5480651654:AAHN90GGJui6qFWJUzS6DByNpCPYrsm83AM'
+        self.bot_token = '5592039815:AAGRRXWh2vNm8T79SOhX1afycZEpRzEVVV4'
         self.admins = (5306348087, 5177494340, 5581082758, 5316104187)
 
 
