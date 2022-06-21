190245ae3dcf9ca76335019b4c68b556c9b1e08f
White <maksfundd2@gmail.com>
2022-06-21 23:08:41 +0300

    fix logs

--- log/logg.py
+++ log/logg.py
@@ -8,7 +8,7 @@ from colorama import Fore
 def get_info(text):
     today = datetime.today()
     today = today.strftime("%d-%m-%Y")
-    file_log = logging.FileHandler(f'Log/logs/log-{today}.log')
+    file_log = logging.FileHandler(f'log/logs/Log-{today}.log')
     console_out = logging.StreamHandler()
 
     logging.basicConfig(handlers=(file_log, console_out), format='[%(asctime)s | %(levelname)s]: %(message)s',
@@ -23,7 +23,7 @@ def get_error(text, file_name=None):
     print(f"{Fore.RED}[ERROR] FILE: {file_name} | " + Fore.WHITE + text)
     logging.basicConfig(
         level=logging.ERROR,
-        filename=f'Log/logs/log-{today}.log',
+        filename=f'log/logs/Log-{today}.log',
         format=u'[%(levelname)s] [%(asctime)s] | %(message)s',
         datefmt="%d-%m-%y %H:%M:%S"
     )
