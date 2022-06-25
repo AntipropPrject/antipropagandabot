f9853c11aff69d1434453833568a5b00fa7effa2
White <maksfundd2@gmail.com>
2022-06-25 17:58:14 +0300

    T-69

--- handlers/donbass_hand.py
+++ handlers/donbass_hand.py
@@ -461,6 +461,6 @@ async def donbas_no_army_here(message: Message, state: FSMContext):
 async def donbas_no_army_here(message: Message, state=FSMContext):
     await state.set_state(TruereasonsState.main)
     nmarkup = ReplyKeyboardBuilder()
-    nmarkup.row(types.KeyboardButton(text="Давай продолжим👉"))
     nmarkup.row(types.KeyboardButton(text="Какой ужас 😨"))
+    nmarkup.row(types.KeyboardButton(text="Давай продолжим👉"))
     await simple_media(message, 'lnr_mobilization', nmarkup.as_markup(resize_keyboard=True))
