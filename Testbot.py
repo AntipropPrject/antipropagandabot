f6333b154818f047f5f1a7a6680144279be785dd
s.kalashnikov <s.kalashnikov@btfcom.com>
2022-06-29 10:28:45 +0300

    webhook

--- Testbot.py
+++ Testbot.py
@@ -76,7 +76,7 @@ def main():
     dp.startup.register(on_startup)
     dp.shutdown.register(on_shutdown)
     app = configure_app(dp, bot)
-    web.run_app(app, host="0.0.0.0", port=80)
+    web.run_app(app, host="0.0.0.0", port=3000)
     # await bot.delete_webhook(drop_pending_updates=True)
     # await dp.start_polling(bot)
 
