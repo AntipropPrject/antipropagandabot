69cc8b9483020f81e4902fe735acf771c3c1d57f
s.kalashnikov <s.kalashnikov@btfcom.com>
2022-07-01 12:47:11 +0300

    webhook

--- handlers/smi_hand.py
+++ handlers/smi_hand.py
@@ -21,13 +21,13 @@ async def smi_statement(message: Message, state: FSMContext):
     messageDict.update({message.from_user.id: message})
 
     person_list = await poll_get(f'Usrs: {message.from_user.id}: Start_answers: who_to_trust_persons:')
+    print(person_list)
 
-    for person in person_list:
-        try:
-            count = (await state.get_data())[f'{person}_gamecount']
-        except:
-            await state.update_data({f'{person}_gamecount': 0})
-            count = 0
+    try:
+        count = (await state.get_data())[f'{person_list[0]}_gamecount']
+    except:
+        await state.update_data({f'{person_list[0]}_gamecount': 0})
+        count = 0
 
     try:
         how_many_rounds = (await data_getter(
@@ -52,13 +52,12 @@ async def smi_statement(message: Message, state: FSMContext):
                 0]
 
             print('aaaaaa', truth_data)
-            await state.update_data({f'{person}_gamecount': count})
+            await state.update_data({f'{person_list[0]}_gamecount': count})
             await state.update_data(truth=truth_data[0], rebuttal=truth_data[5], belive=truth_data[3],
                                     not_belive=truth_data[4], last_media=truth_data[6])
 
         except IndexError as er:
             await message.answer(text=f"Медиафайл не найден {er}")
-
 
         nmarkup = ReplyKeyboardBuilder()
         nmarkup.row(types.KeyboardButton(text="Целенаправленная ложь 👎"))
@@ -151,7 +150,6 @@ async def sme_statement_start_over(message: Message, state: FSMContext):
 
 @router.message(state=propaganda_victim.options, flags=flags)
 async def smi_statement_poll(message: Message, state: FSMContext):
-
     redis = all_data().get_data_red()
 
     list_to_customize = await poll_get(f'Usrs: {message.from_user.id}: Start_answers: who_to_trust_persons:')
