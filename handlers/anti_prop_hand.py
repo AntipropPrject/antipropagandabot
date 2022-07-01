f9e462fe81cc12903aa3fe9f34c81e6c60c8b2ae
s.kalashnikov <s.kalashnikov@btfcom.com>
2022-07-01 13:16:38 +0300

    webhook

--- handlers/anti_prop_hand.py
+++ handlers/anti_prop_hand.py
@@ -555,7 +555,7 @@ async def antip_web_exit_1(message: Message, state: FSMContext):
     redis = all_data().get_data_red()
     for key in redis.scan_iter(f"Usrs: {message.from_user.id}: Start_answers: ethernet:"):
         redis.delete(key)
-    print(await state.get_state())
+    print(f"{await state.get_state()} asdwdqdwqsdqad")
     if await state.get_state() == "propaganda_victim:ppl_propaganda":
         await antip_truth_game_start(message, state)
 
