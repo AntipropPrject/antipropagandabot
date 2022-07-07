9494a12e1c07dcdb8843856a0ff12e953f972f9b
White <maksfundd2@gmail.com>
2022-07-07 18:04:29 +0300

    fix

--- handlers/other/other_file.py
+++ handlers/other/other_file.py
@@ -5,6 +5,6 @@ flags = {"throttling_key": "True"}
 router = Router()
 
 
-@router.message(flags=flags)
+@router.message(content_types='text',flags=flags)
 async def empty(message: types.Message):
     await message.answer("Неправильная команда, вы можете выбрать ответ на клиаватуре или в опросе.\n\n Если желаете начать сначала - нажмите /start")
