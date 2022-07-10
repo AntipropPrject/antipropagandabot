9bfc97f502eb4e66f5e4e92ce0cd4185ed6ab72b
White <maksfundd2@gmail.com>
2022-07-10 19:13:48 +0300

    OTPOR-T-253 | OTPOR-T-234 4/10 | OTPOR-T-255 4/10

--- handlers/other/other_file.py
+++ handlers/other/other_file.py
@@ -5,6 +5,9 @@ flags = {"throttling_key": "True"}
 router = Router()
 
 
-@router.message(content_types='text',flags=flags)
+@router.message(flags=flags)
 async def empty(message: types.Message):
-    await message.answer("Неправильная команда, вы можете выбрать ответ на клиаватуре или в опросе.\n\n Если желаете начать сначала - нажмите /start")
+    if message.content_type == 'pinned_message':
+        await message.delete()
+    else:
+        await message.answer("Неправильная команда, вы можете выбрать ответ на клиаватуре или в опросе.\n\n Если желаете начать сначала - нажмите /start")
