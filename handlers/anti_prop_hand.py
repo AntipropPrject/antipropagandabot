82caf9c31e1dd1b64e9eaaf69abf4b2d64e98c8a
s.kalashnikov <s.kalashnikov@btfcom.com>
2022-07-15 17:56:13 +0300

    Админка для игр

--- handlers/anti_prop_hand.py
+++ handlers/anti_prop_hand.py
@@ -90,8 +90,8 @@ async def antiprop_rather_yes_start_no_tv(message: Message):
     text = await sql_safe_select('text', 'texts', {'name': 'antiprop_rather_yes_start_no_tv'})
     nmarkup = ReplyKeyboardBuilder()
     nmarkup.row(types.KeyboardButton(text="Пропустим этот шаг 👉"))
-    nmarkup.row(types.KeyboardButton(text="Покажи ложь на ТВ - мне интересно посмотреть! 📺"))
-    await message.answer(text, reply_markup=nmarkup.as_markup(resize_keyboard=True))
+    nmarkup.row(types.KeyboardButton(text="Покажи ложь на ТВ — мне интересно посмотреть! 📺"))
+    await message.answer(text, reply_markup=nmarkup.as_markup(resize_keyboard=True), disable_web_page_preview=True)
 
 @router.message(TVPropagandaFilter(option="Да, полностью доверяю"), ((F.text == "Хорошо 👌") |
                 (F.text.contains('явная ложь')) | (F.text.contains('Не знаю, давай'))))
