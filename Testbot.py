83cd8c7e3c9eeea6927d17200ddd389e154691b6
s.kalashnikov <s.kalashnikov@btfcom.com>
2022-07-18 11:25:51 +0300

    Админка для игр

--- Testbot.py
+++ Testbot.py
@@ -9,9 +9,9 @@ from data_base.DBuse import redis_just_one_read
 from day_func import day_count
 from export_to_csv.pg_mg import mongo_export_to_file
 from handlers import start_hand, anti_prop_hand, smi_hand, donbass_hand, true_resons_hand, putin_hand, stopwar_hand, \
-    nazi_hand, preventive_strike, new_admin_hand, welcome_messages, status, main_menu_hand, admin_for_games
+    nazi_hand, preventive_strike, new_admin_hand, welcome_messages, status, main_menu_hand, admin_for_games, admin_for_games_dir
 from export_to_csv import pg_mg
-from handlers.admin_for_games import mistakeorlie
+from handlers.admin_for_games_dir import mistakeorlie
 from handlers.advertising import start_spam
 from handlers.other import other_file
 from middleware.trottling import ThrottlingMiddleware
