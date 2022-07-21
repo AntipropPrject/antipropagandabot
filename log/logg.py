715655340d000fa64ae4e5f4bce8f4ef67228792
White <WilliamHarris9584680@gmail.com>
2022-07-21 22:40:15 +0300

    1.1.5 fix log

--- log/logg.py
+++ log/logg.py
@@ -9,15 +9,7 @@ bot = data.get_bot()
 
 
 def get_info(text):
-    today = datetime.today()
-    today = today.strftime("%d-%m-%Y")
-    file_log = logging.FileHandler(f'log/logs/Log-{today}.log')
-    console_out = logging.StreamHandler()
-
-    logging.basicConfig(handlers=(file_log, console_out), format='[%(asctime)s | %(levelname)s]: %(message)s',
-                        datefmt='%m.%d.%Y %H:%M:%S', level=logging.INFO)
-
-    logging.info(text)
+    pass
 
 
 async def get_error(text, file_name=None):
@@ -32,9 +24,9 @@ async def get_error(text, file_name=None):
         datefmt="%d-%m-%y %H:%M:%S"
     )
 
-    await bot.send_message(chat_id='-1001397216477', text=f"ОШИБКА\n\n"
-                                                          f"___________\n"
-                                                          f"{text}")
+    #await bot.send_message(chat_id='-1001397216477', text=f"ОШИБКА\n\n"
+    #                                                      f"___________\n"
+    #                                                      f"{text}")
 
 
 async def admin_logs(id, name, text):
