be2d7c3baf7c0cac2e3dc7fae2dfe4d7cbd20620
s.kalashnikov <s.kalashnikov@btfcom.com>
2022-07-22 16:51:24 +0300

    Админка для игр

--- Testbot.py
+++ Testbot.py
@@ -127,7 +127,7 @@ async def main():
     dp.startup.register(on_startup)
     dp.shutdown.register(on_shutdown)
     app = configure_app(dp, bot)
-    await web.run_app(app, host="0.0.0.0", port=1443)
+    web.run_app(app, host="0.0.0.0", port=1443)
 
 
 if __name__ == "__main__":
