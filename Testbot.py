058e12698737fee785f580469727f31a7b543832
s.kalashnikov <s.kalashnikov@btfcom.com>
2022-07-22 16:14:57 +0300

    Админка для игр

--- Testbot.py
+++ Testbot.py
@@ -3,13 +3,17 @@ from datetime import datetime
 from aiogram import Dispatcher
 from aiogram.client.session import aiohttp
 from aiogram.dispatcher.fsm.storage.redis import RedisStorage
+from aiogram.dispatcher.webhook.aiohttp_server import SimpleRequestHandler, setup_application
+from aiohttp import web
+
 import bata
 from bata import all_data
 from data_base.DBuse import redis_just_one_read
 from day_func import day_count
 from export_to_csv.pg_mg import mongo_export_to_file
 from handlers import start_hand, anti_prop_hand, smi_hand, donbass_hand, true_resons_hand, putin_hand, stopwar_hand, \
-    nazi_hand, preventive_strike, new_admin_hand, welcome_messages, status, main_menu_hand, admin_for_games, admin_for_games_dir
+    nazi_hand, preventive_strike, new_admin_hand, welcome_messages, status, main_menu_hand, admin_for_games, \
+    admin_for_games_dir
 from export_to_csv import pg_mg
 from handlers.admin_for_games_dir import mistakeorlie
 from handlers.advertising import start_spam
@@ -23,6 +27,36 @@ storage = RedisStorage.from_url(data.redis_url)
 dp = Dispatcher(storage)
 
 
+async def on_startup(dispatcher: Dispatcher) -> None:
+    webhook = await bot.get_webhook_info()
+    if webhook is not None:
+        await bot.delete_webhook(drop_pending_updates=True)
+    else:
+        await bot.set_webhook("https://kamaga777123.xyz/")
+
+    webhook = await bot.get_webhook_info()
+
+    print(webhook)
+    print(webhook)
+    print(webhook)
+
+    print("🚀 Bot launched as Hoook!")
+    print(f"webhook: https://kamaga777123.xyz/")
+
+
+async def on_shutdown(dispatcher: Dispatcher) -> None:
+    print("😴 Bot shutdown...")
+
+    await bot.delete_webhook()
+    await dispatcher.storage.close()
+
+def configure_app(dp, bot) -> web.Application:
+    app = web.Application()
+    SimpleRequestHandler(dispatcher=dp, bot=bot).register(app, path="")
+    setup_application(app, dp, bot=bot)
+    return app
+
+
 async def periodic():
     print('periodic function has been started')
     while True:
@@ -30,7 +64,7 @@ async def periodic():
         status_spam = await redis_just_one_read('Usrs: admins: spam: status:')
         c_time = datetime.now().strftime("%H:%M:%S")
         date = datetime.now().strftime('%Y.%m.%d')
-        #удаление дневного счетчика
+        # удаление дневного счетчика
         if c_time == '21:00:01':
             await day_count(count_delete=True)
         if c_time == '07:00:01':
@@ -84,13 +118,15 @@ async def main():
     session = aiohttp.ClientSession()
     # use the session here
 
-    #periodic function
+    # periodic function
     asyncio.create_task(periodic())
 
     await session.close()
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    # await dp.start_polling(bot)
+
+    app = configure_app(dp, bot)
+    web.run_app(app, host="0.0.0.0", port=1443)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
-
