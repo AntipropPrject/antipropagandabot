b610b9ee7c97fe5ff113cda9897268309bc62d9f
White <WilliamHarris9584680@gmail.com>
2022-08-02 13:03:43 +0300

    fix

--- handlers/story/welcome_messages.py
+++ handlers/story/welcome_messages.py
@@ -50,9 +50,8 @@ async def start_base(message):
     await mongo_user_info(user_id, message.from_user.username)
 
 
-@router.message(welcome_states.start_dialog.dialogue_1, (F.text.contains('верить') | F.text.contains('50 000')),
-                content_types=types.ContentType.TEXT,
-                text_ignore_case=True, flags=flags)  # А с чего мне тебе верить?
+@router.message((F.text.contains('верить') | F.text.contains('50 000')), state=welcome_states.start_dialog.dialogue_1,
+                flags=flags)  # А с чего мне тебе верить?
 async def message_1(message: types.Message, state: FSMContext):
     await mongo_update_stat_new(tg_id=message.from_user.id, column='first_button', value='А с чего мне тебе верить?')
     markup = ReplyKeyboardBuilder()
