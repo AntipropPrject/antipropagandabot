e20202503bc83c8f7eaa92c0079df17672ed68a7
White <WilliamHarris9584680@gmail.com>
2022-08-03 18:33:00 +0300

    OTPOR-T-332 пока один файл

--- handlers/advertising.py
+++ handlers/advertising.py
@@ -5,15 +5,17 @@ from bata import all_data
 from data_base.DBuse import mongo_update_viewed_news
 from datetime import datetime, timedelta
 
-from log.logg import send_to_chat
+from log.logg import send_to_chat, get_logger
 
 router = Router()
 data = all_data()
 bot = data.get_bot()
 
 
+logger = get_logger('SPAM')
 async def start_spam(datet):
     asyncio.create_task(send_to_chat("Рассылка началась"))
+    logger.info("Начало рассылки")
     print('start spam')
     date = datetime.strptime(datet, '%Y.%m.%d %H:%M')
     client = data.get_mongo()
@@ -22,7 +24,13 @@ async def start_spam(datet):
     actual_mass_media = database['spam_actual_news']
     userinfo = database['userinfo']
     main_news_base = all_mass_media_main.find()
-    today_actual = await actual_mass_media.find_one({'datetime': {'$eq': date}})
+    logger.info("Главные новости были получены")
+    try:
+        today_actual = await actual_mass_media.find_one({'datetime': {'$eq': date}})
+        logger.info("Актуальные новости были получены новости были получены")
+    except Exception as e:
+        logger.error(e)
+
     main_news_ids = list()
     main_news_list = list()
     try:
@@ -30,8 +38,10 @@ async def start_spam(datet):
             main_news_list.append(n)
             main_news_ids.append(n['_id'])
     except Exception as e:
+        logger.error(e)
         print(e)
     count = 0
+    logger.info('Начало рассылки')
     async for user in userinfo.find({'datetime_end': {'$lt': datetime.utcnow() - timedelta(days=1)}}):
         if all_data().get_data_red().get(f"user_last_answer: {user['_id']}:") != '1':
             asyncio.create_task(news_for_user(user, main_news_list, today_actual, main_news_ids))
@@ -42,6 +52,7 @@ async def start_spam(datet):
             count += 1
             print("Задача для очереди создана")
         await asyncio.sleep(0.033)
+    logger.info(f'Рассылка завершена, сообщение получили {count}')
     asyncio.create_task(send_to_chat(f"Рассылка завершена\n\nБыло отправлено: +-{count} сообщений"))
 
 
@@ -61,6 +72,7 @@ async def news_for_user(user, main_news_base, today_actual, main_news_ids):
             await send_spam(user_id=user_id, media_id=today_actual['media'], caption=today_actual['caption'])
             print('ОТПРАВКА АКТУАЛЬНОГО СООБЩЕНИЯ', today_actual['media'], today_actual['caption'])
         else:
+            logger.info('Актуальных новостей сегодня нет')
             print('Актуальных новостей сегодня нет')
     else:
         list_not_view = [i for i in main_news_ids if i not in user_viewed_news]
@@ -73,6 +85,7 @@ async def news_for_user(user, main_news_base, today_actual, main_news_ids):
                     print('ЗАПИСЬ АЙДИШНИКА НОВОСТИ В МОНГУ', user_id, list_not_view[0])
 
         else:
+            logger.info('Главные новости для пользователя кончились, а актуальной не было')
             print('Главные новости для пользователя кончились, а актуальной не было')
 
 
@@ -89,6 +102,7 @@ async def send_spam(user_id, media_id, caption):
             except TelegramBadRequest:
                 await bot.send_photo(chat_id=int(user_id), photo=(media_id))
     except TelegramForbiddenError as er:
+        logger.info(f'ПОЛЬЗОВАТЕЛЬ {user_id} -- Заблокировал бота')
         print(f"ПОЛЬЗОВАТЕЛЬ {user_id} -- Заблокировал бота")
 
 
