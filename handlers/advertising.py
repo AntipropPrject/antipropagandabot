1176d64083ca1fb5be3c9e99a5074a9dd64505a0
White <WilliamHarris9584680@gmail.com>
2022-08-05 16:43:42 +0300

    OTPOR-T-318

--- handlers/advertising.py
+++ handlers/advertising.py
@@ -2,7 +2,7 @@ import asyncio
 from aiogram import Router
 from aiogram.exceptions import TelegramBadRequest, TelegramForbiddenError
 from bata import all_data
-from data_base.DBuse import mongo_update_viewed_news
+from data_base.DBuse import mongo_update_viewed_news, mongo_pop_news
 from datetime import datetime, timedelta
 
 from log.logg import send_to_chat, get_logger
@@ -52,6 +52,7 @@ async def start_spam(datet):
             count += 1
             print("Задача для очереди создана")
         await asyncio.sleep(0.033)
+    await mongo_pop_news(m_id=today_actual['media'], coll='actu')
     logger.info(f'Рассылка завершена, сообщение получили {count}')
     asyncio.create_task(send_to_chat(f"Рассылка завершена\n\nБыло отправлено: +-{count} сообщений"))
 
@@ -66,6 +67,7 @@ async def latecomers(user, main_news_base, today_actual, main_news_ids):
 
 async def news_for_user(user, main_news_base, today_actual, main_news_ids):
     user_id = user['_id']
+    print(today_actual)
     user_viewed_news = user['viewed_news']
     if len(user_viewed_news) >= len(main_news_base):
         if today_actual is not None:
@@ -74,6 +76,7 @@ async def news_for_user(user, main_news_base, today_actual, main_news_ids):
         else:
             logger.info('Актуальных новостей сегодня нет')
             print('Актуальных новостей сегодня нет')
+            return
     else:
         list_not_view = [i for i in main_news_ids if i not in user_viewed_news]
         if list_not_view:
