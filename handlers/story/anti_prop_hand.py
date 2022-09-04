e6ece7be8e8422619ef541bb0ad4da88aeb1e8d9
White <WilliamHarris9584680@gmail.com>
2022-09-04 12:27:38 +0300

    OTPOR-T-413 fix next

--- handlers/story/anti_prop_hand.py
+++ handlers/story/anti_prop_hand.py
@@ -84,7 +84,7 @@ async def antip_time_wasted(message: Message):
 @router.message(((F.text == 'В чём подвох? 🤔') | (F.text == 'Я заметил(а)! 😯')), flags=flags)
 async def antip_water_lie(message: Message, state: FSMContext):
     nmarkap = ReplyKeyboardBuilder()
-    nmarkap.row(types.KeyboardButton(text="Продолжай ⏳"))
+    nmarkap.row(types.KeyboardButton(text="Продолжай ⌛"))
     await state.set_state(propaganda_victim.next_2)
     await simple_media(message, 'antip_water_lie', nmarkap.as_markup(resize_keyboard=True))
 
