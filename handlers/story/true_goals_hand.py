52c48158b2b49638b7ded84bd044a289c2935dc0
Borodur <107106869+Brdur@users.noreply.github.com>
2022-09-16 19:45:02 +0300

    OTPOR-T-509

--- handlers/story/true_goals_hand.py
+++ handlers/story/true_goals_hand.py
@@ -1,28 +1,12 @@
-import asyncio
-
 from aiogram import Router, F
 from aiogram import types
 from aiogram.dispatcher.fsm.context import FSMContext
-from aiogram.dispatcher.fsm.state import StatesGroup, State
-from aiogram.exceptions import TelegramBadRequest
-from aiogram.types import Message, ReplyKeyboardRemove
+from aiogram.types import Message
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
-from bot_statistics.stat import mongo_update_stat, mongo_update_stat_new
-from data_base.DBuse import data_getter, sql_safe_select, redis_just_one_write, poll_write, mongo_game_answer
-from data_base.DBuse import redis_delete_from_list
-from filters.MapFilters import OperationWar, WarReason, PoliticsFilter
-from filters.isAdmin import IsAdmin
-from handlers.story import anti_prop_hand
-from handlers.story.nazi_hand import NaziState
-from handlers.story.preventive_strike import PreventStrikeState
-from handlers.story.putin_hand import StateofPutin
-from handlers.story.true_resons_hand import TruereasonsState
-from resources.all_polls import welc_message_one
-from states.donbass_states import donbass_state
+from data_base.DBuse import sql_safe_select
+from filters.MapFilters import OperationWar
 from states.true_goals_states import TrueGoalsState
-from utilts import simple_media
-
 
 flags = {"throttling_key": "True"}
 router = Router()
@@ -37,3 +21,22 @@ async def goals_war_point_now(message: Message, state: FSMContext):
     nmarkup.row(types.KeyboardButton(text="Продолжай ⏳"))
     await message.answer(text, reply_markup=nmarkup.as_markup(resize_keyboard=True), disable_web_page_preview=True)
 
+
+@router.message(OperationWar(answer='(СВО)'), (F.text == "Продолжай ⏳"),
+                state=TrueGoalsState.before_shop, flags=flags)
+async def goals_operation(message: Message, state: FSMContext):
+    await state.set_state(TrueGoalsState.before_shop_operation)
+    text = await sql_safe_select('text', 'texts', {'name': 'goals_operation'})
+    nmarkup = ReplyKeyboardBuilder()
+    nmarkup.row(types.KeyboardButton(text="Продолжай ⏳"))
+    await message.answer(text, reply_markup=nmarkup.as_markup(resize_keyboard=True), disable_web_page_preview=True)
+
+
+@router.message(F.text == "Продолжай ⏳", state=TrueGoalsState.before_shop_operation, flags=flags)
+async def goals_not_operation(message: Message, state: FSMContext):
+    await state.set_state(TrueGoalsState.before_shop)
+    text = await sql_safe_select('text', 'texts', {'name': 'goals_not_operation'})
+    nmarkup = ReplyKeyboardBuilder()
+    nmarkup.row(types.KeyboardButton(text="Хорошо 🤝"))
+    nmarkup.add(types.KeyboardButton(text="*презрительно хмыкнуть* 🤨"))
+    await message.answer(text, reply_markup=nmarkup.as_markup(resize_keyboard=True), disable_web_page_preview=True)
--- states/true_goals_states.py
+++ states/true_goals_states.py
@@ -3,6 +3,7 @@ from aiogram.dispatcher.fsm.state import State, StatesGroup
 
 class TrueGoalsState(StatesGroup):
     before_shop = State()
+    before_shop_operation = State()
     main = State()
     opp_root = State()
     game = State()
