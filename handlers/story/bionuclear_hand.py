d43834aff3dc7e0398ad12d72d52dafd1fd526a6
Borodur <107106869+Brdur@users.noreply.github.com>
2022-10-19 15:47:10 +0300

    био

--- handlers/story/bionuclear_hand.py
+++ handlers/story/bionuclear_hand.py
@@ -13,9 +13,10 @@ from utilts import simple_media
 
 flags = {"throttling_key": "True"}
 router = Router()
-router.message.filter(state=(TrueGoalsState, WarGoalsState,start_dialog.big_story))
+router.message.filter(state=(TrueGoalsState, WarGoalsState, start_dialog.big_story))
 router.poll_answer.filter(state=TrueGoalsState)
 router.message.middleware(Reportware())
+
 
 @router.message(commands=['goals_facts_test'], state='*', flags=flags)
 @router.message((F.text.contains('на факты 👀')), state=TrueGoalsState.power_change, flags=flags)
@@ -26,12 +27,14 @@ async def goals_fact_1(message: Message, state: FSMContext):
     nmarkup.row(types.KeyboardButton(text="Следующий факт 👉"))
     await simple_media(message, 'goals_fact_1', reply_markup=nmarkup.as_markup(resize_keyboard=True))
 
+
 @router.message((F.text.contains('Донбасс ☝')), state=TrueGoalsState.goals_fact_1, flags=flags)
 async def goals_arg(message: Message, state: FSMContext):
     nmarkup = ReplyKeyboardBuilder()
     nmarkup.row(types.KeyboardButton(text="Следующий факт 👉"))
     text = await sql_safe_select('text', 'texts', {'name': 'goals_arg'})
     await message.answer(text, reply_markup=nmarkup.as_markup(resize_keyboard=True), disable_web_page_preview=True)
+
 
 @router.message((F.text.contains('факт 👉')), state=TrueGoalsState.goals_fact_1, flags=flags)
 async def goals_fact_2(message: Message, state: FSMContext):
@@ -41,6 +44,7 @@ async def goals_fact_2(message: Message, state: FSMContext):
     nmarkup.row(types.KeyboardButton(text="Достаточно фактов ✋"))
     await simple_media(message, 'goals_fact_2', reply_markup=nmarkup.as_markup(resize_keyboard=True))
 
+
 @router.message((F.text.contains('факт 👉')), state=TrueGoalsState.goals_fact_2, flags=flags)
 async def goals_fact_3(message: Message, state: FSMContext):
     await state.set_state(TrueGoalsState.goals_fact_3)
@@ -48,6 +52,7 @@ async def goals_fact_3(message: Message, state: FSMContext):
     nmarkup.row(types.KeyboardButton(text="Следующий факт 👉"))
     nmarkup.row(types.KeyboardButton(text="Достаточно фактов ✋"))
     await simple_media(message, 'goals_fact_3', reply_markup=nmarkup.as_markup(resize_keyboard=True))
+
 
 @router.message((F.text.contains('факт 👉')), state=TrueGoalsState.goals_fact_3, flags=flags)
 async def goals_fact_4(message: Message, state: FSMContext):
@@ -69,6 +74,7 @@ async def goals_fact_5(message: Message, state: FSMContext):
     text = await sql_safe_select('text', 'texts', {'name': 'goals_fact_5'})
     await message.answer(text, reply_markup=nmarkup.as_markup(resize_keyboard=True), disable_web_page_preview=True)
 
+
 @router.message((F.text.contains('факт 👉')), state=TrueGoalsState.goals_fact_5, flags=flags)
 async def goals_fact_6(message: Message, state: FSMContext):
     await state.set_state(TrueGoalsState.goals_fact_6)
@@ -79,6 +85,7 @@ async def goals_fact_6(message: Message, state: FSMContext):
     text = await sql_safe_select('text', 'texts', {'name': 'goals_fact_6'})
     await message.answer(text, reply_markup=nmarkup.as_markup(resize_keyboard=True), disable_web_page_preview=True)
 
+
 @router.message((F.text.contains('факт 👉')), state=TrueGoalsState.goals_fact_6, flags=flags)
 async def goals_fact_7(message: Message, state: FSMContext):
     await state.set_state(TrueGoalsState.goals_fact_7)
@@ -87,4 +94,6 @@ async def goals_fact_7(message: Message, state: FSMContext):
     await simple_media(message, 'goals_fact_7', reply_markup=nmarkup.as_markup(resize_keyboard=True))
     import time
     time.sleep(1)
-    await message.answer('У меня есть ещё 9 фактов, но разработчики их ещё не успели добавить. Поэтому давайте продолжим. 🙂', reply_markup=nmarkup.as_markup(resize_keyboard=True), disable_web_page_preview=True)
+    await message.answer(
+        'У меня есть ещё 9 фактов, но разработчики их ещё не успели добавить. Поэтому давайте продолжим. 🙂',
+        reply_markup=nmarkup.as_markup(resize_keyboard=True), disable_web_page_preview=True)
