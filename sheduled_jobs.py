bf44d8e00f6034959bdf1faaf5320247adf37f04
Borodur <107106869+Brdur@users.noreply.github.com>
2022-10-29 17:52:33 +0300

    рассылка

--- sheduled_jobs.py
+++ sheduled_jobs.py
@@ -23,7 +23,6 @@ async def backups():
 
 async def periodic_advs():
     # Включение/выключение можно попробовать сделать через планировщик
-    print("Mailing try")
     status_spam = await redis_just_one_read('Usrs: admins: spam: status:')
     if status_spam == '1':
         print("Mailing")
