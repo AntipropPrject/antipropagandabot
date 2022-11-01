8fc562233e43df31ef3131bf5faeac1bbae3e99c
s.kalashnikov <s.kalashnikov@btfcom.com>
2022-11-01 14:42:51 +0300

    Правки статистики(пробую добавить is_ban)

--- handlers/admin_handlers/admin_statistics.py
+++ handlers/admin_handlers/admin_statistics.py
@@ -27,17 +27,21 @@ async def pretty_progress_stats():
     day_unt = await mongo_count_docs('database', 'statistics_new',
                                      {"datetime": {"$gte": past}}, check_default_version=False)
     stat = await mongo_count_docs('database', 'statistics_new', {"come": {"$exists": True}})
+    is_ban = await mongo_count_docs('database', 'userinfo', {"is_ban": {"$exists": True}})
     stat_statistics = await mongo_count_docs('database', 'statistics_new',
                                              {"datetime": {"$gte": release_date['v3.1']}}, check_default_version=False)
-    is_ban = await mongo_count_docs('database', 'userinfo',
-                                    {"datetime": {"$gte": release_date['v3.1']}}, check_default_version=False)
+    is_ban_afterFlags = await mongo_count_docs('database', 'userinfo',
+                                               {"is_ban": {'$exists': True},
+                                                "datetime": {"$gte": release_date['v3.1']}},
+                                               check_default_version=False)
     text = ""
     for point in stat_points:
         users_count = await mongo_count_docs('database', 'statistics_new',
                                              {stat_points[point]: {'$exists': True},
                                               "datetime": {"$gte": release_date['v3.1']}}, check_default_version=False)
         text += count_visual(stat_statistics, users_count, point)
-    text += count_visual(stat, is_ban, 'Забанили бота')
+    text += count_visual(stat_statistics, is_ban_afterFlags, 'Забанили бота (после установки всех флагов)')
+    text += count_visual(stat, is_ban, 'Забанили бота(всего)')
     text = f"<code>Всего пользователей: {stat}\n" \
            f"Пользователей после установки всех флагов: {stat_statistics}\n" \
            f"Новых пользователей за сутки: {day_unt}\n\n</code>" \
